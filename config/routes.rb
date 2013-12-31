fd4725681f976f3c4f5ff56314c37be8eefe5a85
wddwxy <wddwxy@localhost.localdomain>
2013-12-31 09:53:26 +0800

    static_page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 WeiboApp::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/help"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
