63ec69bfbc1b024a448eb28caffaf5c59ac8ec83
wddwxy <wxy2008@yeah.net>
2014-01-14 01:59:47 +0800

    finish following

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,14 @@ WeiboApp::Application.routes.draw do
   match '/signup',   to: 'users#new',   via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
-  resources :users
-  resources :sessions, only: [:new, :create, :destroy]
-  resources :microposts, only: [:create, :destroy]
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :sessions,      only: [:new, :create, :destroy]
+  resources :microposts,    only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
